90c27e0c386c0c492edc9c475dbcaa376f0ca77d
tmendes <tiago743@yahoo.com.br>
2024-06-13 20:25:32 -0300

    inicio do meu primeiro app real

--- conta-banco/conta-banco/src/ContaTerminal.java
+++ conta-banco/conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        
+    Scanner dados = new Scanner(System.in);
+        
+        System.out.println("Por favor, digite o nome!");
+        String nome = dados.nextLine();
+        
+        System.out.println("Por favor, digite a agência!");
+        String agencia = dados.next();
+
+        System.out.println("Por favor, digite o número da conta!");
+        short conta = (short) dados.nextInt();
+        
+        System.out.println("Por favor, digite o sTaldo!");
+        double saldo = dados.nextDouble();
+
+        
+        //imprimindo os dados obtidos pelo usuario
+        System.out.println("Olá " + nome + " obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo R$" + saldo + " já está disponível para saque!");
+     
+        dados.close();
     }
 }
