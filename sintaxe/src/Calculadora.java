62ae1a9735b723d95c4fe033ab0b70ba5dac9d01
tmendes <tiago743@yahoo.com.br>
2024-06-13 19:33:58 -0300

    final

---
+++ sintaxe/src/Calculadora.java
@@ -0,0 +1,21 @@
+/**
+* 
+* @author  Tiago Mendes
+* @version 1.0
+* @since   01/01/2022
+*
+*/
+
+public class Calculadora {
+
+   /**
+   * Este método é utilizado para somar dois números inteiros
+   * @param numeroUm este é o primeiro parâmetro do método
+   * @param numeroDois este é o segundo parâmetro do método
+   * @return int o resultado deste método é a soma dos dois números.
+   */    
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+
+}
